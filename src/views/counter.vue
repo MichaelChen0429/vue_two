581a804e766b7774f1af518e1a743de29b6de6c9
07170203 <07170203@gm.scu.edu.tw>
2021-01-18 20:25:55 +0800

    vue

---
+++ src/views/counter.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h1>This is a counter app</h1>
+  </div>
+  <div id="center">
+    <button v-on:click="minus()" class="whf1">-</button>
+    <button v-bind:class='{disable: is_disabled}' class="whf2" >{{ counter }}</button>
+    <button v-on:click="plus()" class="whf3">+</button>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      counter: 0
+    }
+  },
+  computed: {
+    is_disabled () {
+      return this.counter <= 0 || this.counter >= 10
+    }
+  },
+  methods: {
+    minus: function () {
+      if (this.counter > 0) {
+        this.counter = this.counter - 1
+      }
+    },
+    plus: function () {
+      if (this.counter < 10) {
+        this.counter = this.counter + 1
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+  #center {
+    text-align: center;
+}
+.whf1 {
+    width:40px;
+    height:40px;
+    font-size:20px;
+}
+.whf2 {
+    width:80px;
+    height:40px;
+    font-size:20px;
+}
+.whf3 {
+    width:40px;
+    height:40px;
+    font-size:20px;
+}
+.disable{
+  background-color: #faa
+}
+</style>
---
+++ src/views/todo.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>This is an todo app</h1>
+  </div>
+</template>
+
+<script>
+
+</script>
